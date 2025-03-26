59a3d8f8c9463bf6bbd4c6d0f6c6e34d036314ee
juan.castillo <s37q55rktnwxlu52fc4derkhyij2rv5rw4r33rqdahdv5odnjumq>
2025-03-26 17:40:26 -0500

    Se creo la pagina para hacer login

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ import logging
 import os
 import sys
 
-# Agregar el directorio del proyecto al sys.path
-# Esto permite que Python encuentre los módulos en el directorio del proyecto.
-sys.path.append(os.path.dirname(__file__))
-
 # Configuración del logging
 # Definir el directorio donde se guardarán los archivos de log.
 log_dir = os.path.join(os.path.dirname(__file__), 'logs')
@@ -26,7 +22,7 @@ log_file = os.path.join(log_dir, 'automation.log')
 #   - StreamHandler: Muestra los logs en la consola.
 logging.basicConfig(
     level=logging.DEBUG,
-    format='%(asctime)s - %(name)-30s - %(levelname)-8s - %(message)s',
+    format='%(asctime)s - %(name)-45s - %(levelname)-8s - %(message)s',
     handlers=[
         logging.FileHandler(log_file),
         logging.StreamHandler(sys.stdout)
@@ -37,24 +33,40 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # Importar la función open_browser desde el módulo scripts.open_browser.
-from scripts.open_browser import open_browser
+from Scripts.open_browser import open_browser
+from Scripts.close_browser import close_browser
+from Scripts.login_browser import login
+
 
 def main():
+    """
+    Función principal que inicia el script de automatización.
+    """
     try:
-        # Registrar un mensaje de información indicando el inicio del script.
         logger.info("Starting the automation script")
         
         # URL a abrir
-        url = "https://www.example.com"
+        url = "https://acme-test.uipath.com/login"
         
-        # Llamar a la función open_browser con la URL proporcionada.
-        open_browser(url)
+        # Nombre del navegador a usar (por ejemplo, 'chrome' o 'firefox').
+        browser_name = "chrome"
         
-        # Registrar un mensaje de información indicando que el script se completó con éxito.
+        # Llamar a la función open_browser con la URL y el nombre del navegador.  
+        driver = open_browser(url, browser_name)
+
+        # Credenciales de login
+        username = "jmmana@gmail.com"
+        password = "P@ssWoor!d"
+
+        # Llamar a la función de login
+        login(driver, username, password)
+
+        # Cerrar el navegador
+        close_browser(driver)
+
+
         logger.info("Automation script completed successfully")
     except Exception as e:
-        # Registrar un mensaje de error si ocurre una excepción.
-        # exc_info=True incluye la traza del error en el log.
         logger.error("An error occurred in the main script", exc_info=True)
 
 # Si el script se ejecuta directamente (no importado como módulo), llamar a la función main.
