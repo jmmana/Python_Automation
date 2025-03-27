40c422a912a484471572484d0ba9bc1ca8b1ed79
juan.castillo <s37q55rktnwxlu52fc4derkhyij2rv5rw4r33rqdahdv5odnjumq>
2025-03-27 13:40:46 -0500

    Carga Inicial

---
+++ main_dispatcher.py
@@ -0,0 +1,73 @@
+import logging
+import os
+import sys
+import pandas as pd
+from Scripts.config_environment import load_environment  # Importar la función de configuración del entorno
+
+# Cargar las variables de entorno
+env_vars = load_environment()
+
+# Acceder a las variables de entorno desde el diccionario
+db_file = env_vars['DB_FILE']
+table_name = env_vars['TABLE_NAME']
+login_url = env_vars['LOGIN_URL']
+work_items_url = env_vars['WORK_ITEMS_URL']
+process_base_url = env_vars['PROCESS_BASE_URL']
+browser_name = env_vars['BROWSER_NAME']
+username = env_vars['WEB_USERNAME']
+password = env_vars['WEB_PASSWORD']
+
+# Configuración del logging
+log_dir = os.path.join(os.path.dirname(__file__), 'logs')
+os.makedirs(log_dir, exist_ok=True)
+log_file = os.path.join(log_dir, 'automation.log')
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s - %(name)-45s - %(levelname)-8s - %(message)s',
+    handlers=[
+        logging.FileHandler(log_file),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+logger = logging.getLogger(__name__)
+
+from Scripts.open_browser import open_browser
+from Scripts.close_browser import close_browser
+from Scripts.login_browser import login
+from Scripts.datascraping_browser import scrape_work_items
+from Scripts.processdata_browser import save_to_csv
+from Scripts.processdata_browser import save_to_sqlite
+
+def main():
+    """
+    Función principal que inicia el script de automatización.
+    """
+    try:
+        logger.info("Starting the automation script")
+        
+        driver = open_browser(login_url, browser_name)
+
+        # Llamar a la función de login
+        login(driver, username, password)
+
+        # Llamar a la función de scraping
+        df = scrape_work_items(driver, work_items_url)
+
+        # Guardar los datos en un archivo CSV en el directorio Data
+        output_directory = "Data"
+        save_to_csv(df, output_directory)
+
+        # Guardar los datos en la base de datos SQLite
+        save_to_sqlite(df, db_file, table_name)
+        
+        # Cerrar el navegador
+        close_browser(driver)
+
+        logger.info("Automation script completed successfully")
+    except Exception as e:
+        logger.error("An error occurred in the main script", exc_info=True)
+
+if __name__ == "__main__":
+    main()
